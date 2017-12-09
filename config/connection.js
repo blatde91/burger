b6bd48057133ce31b4f317c3940d02ea8f6a9bc5
Daniel Blat <blatde91@gmail.com>
2017-12-08 23:38:30 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection({
 	  host: "localhost",
 	  user: "root",
-	  password: "Loveless91",
+	  password: "nope",
 	  database: "burgers_db"
 	});
 }
